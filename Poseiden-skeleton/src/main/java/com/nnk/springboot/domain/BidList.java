53bc34e364e6f322cba88aad703e76032302e5c0
Esteban <est.savalle@gmail.com>
2022-04-07 13:47:56 +0200

    First push develop

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -7,9 +7,35 @@ import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "bidlist")
 public class BidList {
     // TODO: Map columns in data table BIDLIST with corresponding java fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer BidListId;
+    String account;
+    String type;
+    Double bidQuantity;
+    Double askQuantity;
+    Double bid;
+    Double ask;
+    String benchmark;
+    LocalDateTime bidListDate;
+    String commentary;
+    String security;
+    String status;
+    String trader;
+    String book;
+    String creationName;
+    LocalDateTime creationDate;
+    String revisionName;
+    LocalDateTime revisionDate;
+    String dealName;
+    String dealType;
+    String sourceListId;
+    String side;
+
 }
